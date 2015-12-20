bbf72402efcbf170cdb22111f582e07d8169db64
Maximilian Raab <raab.maximilian@gmail.com>
2015-12-20 11:33:13 +0100

    Feature complete Priming

--- src/Mailing/HtmlPriming.php
+++ src/Mailing/HtmlPriming.php
@@ -12,89 +12,102 @@ class HtmlPriming
     public function GetHtmlPriming()
     {
         $db = DBAccessSingleton::getInstance();
-        $name = $db->username;
+
         $message = SingletonMessage::Instance();
         $cid = $message->embed(Swift_Image::fromPath('assets/priming_eisbaer.png'));
 
-        $calc = new Calculations();
-
-        $eClass = $calc->GetEfficiencyClass($calc->CalcEnergyUsageUser());
-        if($eClass == 'a')
+        if($db->extractionUserCount >= 200)
         {
-            // take this picture
+            $energyConsumption = round(array_sum($db->energyUser) / 1000,1);
+            $textConsumption = 'with your last <b>200</b> showers you have consumed <b>' . $energyConsumption .'</b> kWh energy. ';
         }
-        elseif($eClass == 'c')
+        else
         {
-            // take that other picture
+            $energyConsumption = (array_sum($db->energyUser) / count($db->energyUser)) * 200 / 1000;
+            $textConsumption = "with your predicted <b>200</b> showers you will consume <b>" . round($energyConsumption,1) ."</b> kWh energy. ";
+        }
+
+
+        $textConsEqual = 'Your energy usage corresponds to the annual energy consumption of ';
+        if($energyConsumption > 0 && $energyConsumption < 165)
+        {
+            $textConsEqual = $textConsEqual . '<b>2 laptops</b>.';
+        }
+        elseif($energyConsumption >= 165 && $energyConsumption < 240)
+        {
+            $textConsEqual = $textConsEqual . '<b>1 washing machine</b>.';
+        }
+        elseif($energyConsumption >= 240 && $energyConsumption < 330)
+        {
+            $textConsEqual = $textConsEqual . '<b>1 fridge</b>.';
+        }
+        elseif($energyConsumption >= 330 && $energyConsumption < 460)
+        {
+            $textConsEqual = $textConsEqual . '<b>2 laptops</b>.';
+        }
+        elseif($energyConsumption >= 460)
+        {
+            $textConsEqual = $textConsEqual . '<b>'. round($energyConsumption/350,1) . ' times your energy usage for lighting</b>.';
         }
 
         return
 
-            '<table cellpadding="0" cellspacing="0">
+'<table cellpadding="0" cellspacing="0">
     <tr>
-        <td class="pattern" width="800" align="center">
-            <table class="content-shadow" cellpadding="0" cellspacing="0">
+        <td  width="800" align="center">
+            <table cellpadding="0" cellspacing="0">
                 <tr>
                     <td class="col" width="200" valign="top">
                         <table cellpadding="0" cellspacing="0">
                             <tr>
-                                <td class="hero_image"><img src="assets/priming_eisbaer.png" width="200" alt="" style="display: block; border: 0;" /></td>
-                     <!--           <td class="hero_image"><img src="'. $cid  .'" width="200" alt="" style="display: block; border: 0;" /></td> -->
+                                <a class="hero_image"><a href="http://amphiro.com/"><img src="assets/logo.png" width="200" alt="" style="display: block; border: 0;" /> </a></td>
                             </tr>
-
                         </table>
                     </td>
                     <td class="spacer" width="20" style="font-size: 1px;">&nbsp;</td>
-                    <td class="col" width="400" valign="top">
+                    <td class="col" width="600" valign="top">
                         <table cellpadding="0" cellspacing="0">
-
                             <tr>
-                                <td class="headline" align="left" style="font-family: arial,sans-serif; font-size: 22px; color: #333; padding-top: 15px;">
+                                <td width="400" class="headline" align="left" style="font-family: arial,sans-serif; font-size: 22px; color: #333; padding-top: 15px;">
                                     Together we are can save his world!
-                                    <hr/>
+                                </td>
+                                <td align="center" width="200" class="hero_image"><img src="assets/badges/_baer.png" height="40" width="auto" alt="" style="display: block; border: 0;" />
                                 </td>
                             </tr>
+                            <tr><td colspan="2" ><hr/></td></tr>
                             <tr>
-                                <td class="body_copy" align="left" style="font-family: arial,sans-serif; font-size: 14px; line-height: 20px !important; color: #7f7f7f; padding-top: 10px;">
-                                    Hallo ' .  $name .'! <br/>
-                                    We are pleased to provide you this personal report to help reduce energy consumption.
+                                <td width="400" class="body_copy" align="left" style="font-family: arial,sans-serif; font-size: 14px; line-height: 20px !important; color: #7f7f7f; padding-top: 10px;">
+                                    Hallo ' . $db->firstname .'! <br/>
+                                    We are pleased to provide you your <b>personal</b> report. We want to help you to reduce your energy consumption.
                                     <br/>
-                                    The purpose of this report is to:
+                                    With this report we provide you:
                                     <lu>
-                                        <li><strong>provide information</strong></li>
-                                        <li><strong>help to track your progress</strong></li>
-                                        <li><strong>share energy saving tips</strong></li>
+                                        <li><strong>your energy consumption compared to our customers</strong></li>
+                                        <li><strong>personal energy saving tips</strong></li>
+                                        <li><strong>your energy saving progress</strong></li>
                                     </lu>
                                 </td>
-                            </tr>
-                         </table>
-                    </td>
+                                <td class="body_copy" align="left" style="font-family: arial,sans-serif; font-size: 14px; line-height: 20px !important; color: #7f7f7f; padding-top: 10px; padding-left: 10px" width="200">
 
-                    <td class="spacer" width="20" style="font-size: 1px;">&nbsp;</td>
-
-                    <td class="col" width="200" valign="top">
-                        <table cellpadding="0" cellspacing="0">
-                            <tr>
-                                <td class="hero_image">
-
-                                <img src="assets/logo.png" width="200" alt="" style="display: block; border: 0;" />
-
-                                &nbsp;
-                                <hr/>
-                                '. $db->email .' <br/>
+                                Our greatest customer<br/>
                                 '. $db->firstname . '  '. $db->familyname .' <br/>
+                                '. $db->email .' <br/>
                                 '. $db->address.' <br/>
                                 '. $db->city .' <br/>
-
-                               <!-- link auf amphiro oder einer anderen health care seite -->
-
+                                <br/>
+                                <br/>
                                 </td>
                             </tr>
 
-                        </table>
+                         </table>
                     </td>
                 </tr>
             </table>
+        </td>
+     </tr>
+       <tr>
+        <td width="800" align="left" class="body_copy" style="font-family: arial,sans-serif; font-size: 17px; line-height: 20px !important; color: #7f7f7f; padding-top: 20px; padding-left: 5px" >
+            '. $textConsumption . $textConsEqual .'
         </td>
     </tr>
 </table>
